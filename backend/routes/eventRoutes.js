33d6c3f77abd28e28e21b8f3fa0b23eb25a1e75f
WilWilbert123 <pokoygamis@example.com>
2026-03-21 13:56:45 +0800

    refactor: optimize event data processing and response formatting

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
- 
-const { createEvent, getAllEvents } = require('../controllers/eventController'); 
-const { protect } = require('../middleware/authMiddleware');
 
+ 
+const eventCtrl = require('../controllers/eventController');
+const authMid = require('../middleware/authMiddleware');
+
+ 
+console.log("LOG: Controller methods found:", Object.keys(eventCtrl));
+console.log("LOG: Middleware methods found:", Object.keys(authMid));
+
+const { createEvent, getAllEvents } = eventCtrl;
+const { protect } = authMid;
+
+ 
 router.post('/host', protect, createEvent);
-
- 
-router.get('/', protect, getAllEvents); 
+router.get('/', protect, getAllEvents);
 
 module.exports = router;
