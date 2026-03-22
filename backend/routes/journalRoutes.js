9ad9330cc2bbf0235aabc690df08cd3367eafece
WilWilbert123 <pokoygamis@example.com>
2026-03-22 13:26:18 +0800

    feat: refine journal API endpoints for global feed and private access

--- backend/routes/journalRoutes.js
+++ backend/routes/journalRoutes.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const { getJournals, createJournal, deleteJournal,removeJournalMedia,getGlobalJournals } = require('../controllers/journalController');
+const { 
+    getJournals, 
+    createJournal, 
+    deleteJournal, 
+    removeJournalMedia, 
+    getGlobalJournals 
+} = require('../controllers/journalController');
 
-router.get('/:userId', getJournals);
-router.post('/', createJournal);
-router.delete('/:id', deleteJournal);
-router.patch('/:id/media', removeJournalMedia);
+
 router.get('/global', protect, getGlobalJournals);
+router.get('/:userId', protect, getJournals); 
+ 
+router.post('/', protect, createJournal);
+router.delete('/:id', protect, deleteJournal);
+router.patch('/:id/media', protect, removeJournalMedia);
+
 module.exports = router;
